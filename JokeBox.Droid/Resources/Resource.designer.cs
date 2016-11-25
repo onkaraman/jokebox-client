1c75ebb9eaee6c8ac5fcc969a6c768a26e31da1c
Onur Karaman <onur@areondev.de>
2016-11-25 12:34:57 +0100

    Main.axml: Added progress bar.

--- JokeBox.Droid/Resources/Resource.designer.cs
+++ JokeBox.Droid/Resources/Resource.designer.cs
@@ -1477,23 +1477,23 @@ namespace JokeBox.Droid
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f0b0055
-			public const int BodyLayout = 2131427413;
-			
-			// aapt resource value: 0x7f0b0057
-			public const int BodyScrollView = 2131427415;
-			
-			// aapt resource value: 0x7f0b005a
-			public const int Downvotes = 2131427418;
-			
-			// aapt resource value: 0x7f0b0054
-			public const int HeaderPointsUnit = 2131427412;
-			
-			// aapt resource value: 0x7f0b0053
-			public const int HeaderPointsValue = 2131427411;
+			// aapt resource value: 0x7f0b0056
+			public const int BodyLayout = 2131427414;
 			
 			// aapt resource value: 0x7f0b0058
-			public const int JokeText = 2131427416;
+			public const int BodyScrollView = 2131427416;
+			
+			// aapt resource value: 0x7f0b005b
+			public const int Downvotes = 2131427419;
+			
+			// aapt resource value: 0x7f0b0055
+			public const int HeaderPointsUnit = 2131427413;
+			
+			// aapt resource value: 0x7f0b0054
+			public const int HeaderPointsValue = 2131427412;
+			
+			// aapt resource value: 0x7f0b0059
+			public const int JokeText = 2131427417;
 			
 			// aapt resource value: 0x7f0b0051
 			public const int MainHeaderLayout = 2131427409;
@@ -1501,17 +1501,20 @@ namespace JokeBox.Droid
 			// aapt resource value: 0x7f0b0050
 			public const int MainLayout = 2131427408;
 			
+			// aapt resource value: 0x7f0b0053
+			public const int MainLogo = 2131427411;
+			
 			// aapt resource value: 0x7f0b0052
-			public const int MainLogo = 2131427410;
+			public const int MainProgressBar = 2131427410;
 			
-			// aapt resource value: 0x7f0b0056
-			public const int QuotationMarks = 2131427414;
-			
-			// aapt resource value: 0x7f0b005c
-			public const int Upvotes = 2131427420;
+			// aapt resource value: 0x7f0b0057
+			public const int QuotationMarks = 2131427415;
 			
 			// aapt resource value: 0x7f0b005d
-			public const int action0 = 2131427421;
+			public const int Upvotes = 2131427421;
+			
+			// aapt resource value: 0x7f0b005e
+			public const int action0 = 2131427422;
 			
 			// aapt resource value: 0x7f0b0041
 			public const int action_bar = 2131427393;
@@ -1537,8 +1540,8 @@ namespace JokeBox.Droid
 			// aapt resource value: 0x7f0b0042
 			public const int action_context_bar = 2131427394;
 			
-			// aapt resource value: 0x7f0b0061
-			public const int action_divider = 2131427425;
+			// aapt resource value: 0x7f0b0062
+			public const int action_divider = 2131427426;
 			
 			// aapt resource value: 0x7f0b0002
 			public const int action_menu_divider = 2131427330;
@@ -1570,14 +1573,14 @@ namespace JokeBox.Droid
 			// aapt resource value: 0x7f0b002b
 			public const int buttonPanel = 2131427371;
 			
-			// aapt resource value: 0x7f0b005e
-			public const int cancel_action = 2131427422;
+			// aapt resource value: 0x7f0b005f
+			public const int cancel_action = 2131427423;
 			
 			// aapt resource value: 0x7f0b0039
 			public const int checkbox = 2131427385;
 			
-			// aapt resource value: 0x7f0b0064
-			public const int chronometer = 2131427428;
+			// aapt resource value: 0x7f0b0065
+			public const int chronometer = 2131427429;
 			
 			// aapt resource value: 0x7f0b001c
 			public const int collapseActionView = 2131427356;
@@ -1606,8 +1609,8 @@ namespace JokeBox.Droid
 			// aapt resource value: 0x7f0b0019
 			public const int end = 2131427353;
 			
-			// aapt resource value: 0x7f0b0069
-			public const int end_padder = 2131427433;
+			// aapt resource value: 0x7f0b006a
+			public const int end_padder = 2131427434;
 			
 			// aapt resource value: 0x7f0b0025
 			public const int expand_activities_button = 2131427365;
@@ -1630,14 +1633,14 @@ namespace JokeBox.Droid
 			// aapt resource value: 0x7f0b0026
 			public const int image = 2131427366;
 			
-			// aapt resource value: 0x7f0b0068
-			public const int info = 2131427432;
+			// aapt resource value: 0x7f0b0069
+			public const int info = 2131427433;
 			
-			// aapt resource value: 0x7f0b0062
-			public const int line1 = 2131427426;
+			// aapt resource value: 0x7f0b0063
+			public const int line1 = 2131427427;
 			
-			// aapt resource value: 0x7f0b0066
-			public const int line3 = 2131427430;
+			// aapt resource value: 0x7f0b0067
+			public const int line3 = 2131427431;
 			
 			// aapt resource value: 0x7f0b0009
 			public const int listMode = 2131427337;
@@ -1645,14 +1648,14 @@ namespace JokeBox.Droid
 			// aapt resource value: 0x7f0b0028
 			public const int list_item = 2131427368;
 			
-			// aapt resource value: 0x7f0b0059
-			public const int mainDownvoteBox = 2131427417;
+			// aapt resource value: 0x7f0b005a
+			public const int mainDownvoteBox = 2131427418;
 			
-			// aapt resource value: 0x7f0b005b
-			public const int mainUpvoteBox = 2131427419;
+			// aapt resource value: 0x7f0b005c
+			public const int mainUpvoteBox = 2131427420;
 			
-			// aapt resource value: 0x7f0b0060
-			public const int media_actions = 2131427424;
+			// aapt resource value: 0x7f0b0061
+			public const int media_actions = 2131427425;
 			
 			// aapt resource value: 0x7f0b001a
 			public const int middle = 2131427354;
@@ -1753,8 +1756,8 @@ namespace JokeBox.Droid
 			// aapt resource value: 0x7f0b0017
 			public const int src_over = 2131427351;
 			
-			// aapt resource value: 0x7f0b005f
-			public const int status_bar_latest_event_content = 2131427423;
+			// aapt resource value: 0x7f0b0060
+			public const int status_bar_latest_event_content = 2131427424;
 			
 			// aapt resource value: 0x7f0b004c
 			public const int submit_area = 2131427404;
@@ -1762,17 +1765,17 @@ namespace JokeBox.Droid
 			// aapt resource value: 0x7f0b000b
 			public const int tabMode = 2131427339;
 			
-			// aapt resource value: 0x7f0b0067
-			public const int text = 2131427431;
+			// aapt resource value: 0x7f0b0068
+			public const int text = 2131427432;
 			
-			// aapt resource value: 0x7f0b0065
-			public const int text2 = 2131427429;
+			// aapt resource value: 0x7f0b0066
+			public const int text2 = 2131427430;
 			
 			// aapt resource value: 0x7f0b0034
 			public const int textSpacerNoButtons = 2131427380;
 			
-			// aapt resource value: 0x7f0b0063
-			public const int time = 2131427427;
+			// aapt resource value: 0x7f0b0064
+			public const int time = 2131427428;
 			
 			// aapt resource value: 0x7f0b002a
 			public const int title = 2131427370;
