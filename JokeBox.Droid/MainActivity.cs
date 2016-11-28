49ca611030de8e0ea0a96dec49c9fc4e3497511c
Onur Karaman <onur@areondev.de>
2016-11-28 09:53:21 +0100

    Main: Loading and voting jokes.

--- JokeBox.Droid/MainActivity.cs
+++ JokeBox.Droid/MainActivity.cs
@@ -19,6 +19,7 @@ namespace JokeBox.Droid
           ScreenOrientation = Android.Content.PM.ScreenOrientation.Portrait)]
     public class MainActivity : Activity
     {
+        private int _jokeIndex;
         private ProgressBar _progBar;
         private TextView _pointsText;
         private TextView _jokeContent;
@@ -32,8 +33,12 @@ namespace JokeBox.Droid
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.Main);
+
+            _jokeIndex = 0;
+
             setupViews();
             assignEvents();
+            hideJokeUI();
             getJokes();
         }
 
@@ -56,16 +61,21 @@ namespace JokeBox.Droid
         /// </summary>
         private void assignEvents()
         {
-            _upvoteBox.Click += updateClicked;
+            _upvoteBox.Click += upvoteClicked;
             _downvoteBox.Click += downvoteClicked;
         }
 
         /// <summary>
         /// Will upvote the current joke.
         /// </summary>
-        private void updateClicked (object sender, System.EventArgs e)
+        private void upvoteClicked (object sender, System.EventArgs e)
         {
-
+            ThreadPool.QueueUserWorkItem(o =>
+            {
+                APIAccessor.Static.Vote(true, _jokes[_jokeIndex].id, "DE");
+            });
+            _jokeIndex += 1;
+            showJoke();
         }
 
         /// <summary>
@@ -73,7 +83,12 @@ namespace JokeBox.Droid
         /// </summary>
         private void downvoteClicked(object sender, System.EventArgs e)
         {
-
+            ThreadPool.QueueUserWorkItem(o =>
+            {
+                APIAccessor.Static.Vote(false, _jokes[_jokeIndex].id, "DE");
+            });
+            _jokeIndex += 1;
+            showJoke();
         }
 
         /// <summary>
@@ -85,6 +100,47 @@ namespace JokeBox.Droid
             {
                 string raw = await APIAccessor.Static.Get("", "DE");
                 _jokes = APIParser.Static.ParseGet(raw);
+                showJoke();
+            });
+        }
+
+        /// <summary>
+        /// Will hide the joke UI (content, vote buttons).
+        /// </summary>
+        private void hideJokeUI()
+        {
+            _jokeContent.Visibility = Android.Views.ViewStates.Invisible;
+            _upvoteBox.Visibility = Android.Views.ViewStates.Invisible;
+            _downvoteBox.Visibility = Android.Views.ViewStates.Invisible;
+        }
+
+        private void showJokeUI()
+        {
+            _jokeContent.Visibility = Android.Views.ViewStates.Visible;
+            _upvoteBox.Visibility = Android.Views.ViewStates.Visible;
+            _downvoteBox.Visibility = Android.Views.ViewStates.Visible;
+        }
+
+        /// <summary>
+        /// Will fade in the joke UI.
+        /// </summary>
+        private void fadeInJokeUI()
+        {
+        }
+
+        /// <summary>
+        /// Will show the current joke.
+        /// </summary>
+        private void showJoke()
+        {
+            RunOnUiThread(() =>
+            {
+                showJokeUI();
+                _progBar.Visibility = Android.Views.ViewStates.Invisible;
+
+                _jokeContent.Text = _jokes[_jokeIndex].content;
+                _upvotes.Text = _jokes[_jokeIndex].upvotes.ToString();
+                _downvotes.Text = _jokes[_jokeIndex].downvotes.ToString();
             });
         }
     }
